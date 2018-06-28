fc34e34776817b4f6a9051e2e354f21a11c41676
Nicolás Alejandro Ferrero <nicolasferreroutn@gmail.com>
2018-06-28 15:15:32 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP4/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class TestUnitario
+    {
+        [TestMethod]
+        public void ListaInstanciada()
+        {
+            Correo correo = new Correo();
+            Assert.IsNotNull(correo.Paquetes);
+        }
+
+        [TestMethod]
+        public void PaquetesDiferentes()
+        {
+            Paquete p1 = new Paquete("aaabbb", "1234");
+            Paquete p2 = new Paquete("cccddd", "1234");
+            Correo correo = new Correo();
+            correo += p1;
+
+            try
+            {
+                correo += p2;
+            }
+            catch
+            {
+                Assert.Fail("Mismo trackingID");
+
+            }      
+        }
+    }
+}
