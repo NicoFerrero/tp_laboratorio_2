76cc17a39b63aee02d15bc6123a3dbbadd169f1b
Nicolás Alejandro Ferrero <nicolasferreroutn@gmail.com>
2018-07-03 10:11:44 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP4Corregido/MainCorreo/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+namespace MainCorreo
+{
+    public partial class FrmPpal : Form
+    {
+        private Correo _correo;
+        
+        public FrmPpal()
+        {
+            InitializeComponent();
+            this._correo = new Correo();
+        }
+
+        private void ActualizaEstado()
+        {
+            this.lstEstadoIngresados.Items.Clear();
+            this.lstEstadoEnViaje.Items.Clear();
+            this.lstEstadoEntregado.Items.Clear();
+
+            foreach (Paquete paquete in _correo.Paquetes)
+            {
+                switch (paquete.Estado)
+                {
+                    case Paquete.EEstado.Ingresado:
+                        this.lstEstadoIngresados.Items.Add(paquete);
+                        break;
+
+                    case Paquete.EEstado.EnViaje:
+                        this.lstEstadoEnViaje.Items.Add(paquete);
+                        break;
+
+                    case Paquete.EEstado.Entregado:
+                        this.lstEstadoEntregado.Items.Add(paquete);
+                        break;
+                }
+            }
+        }
+
+        private void paq_InformaEstado(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                Paquete.DelegadoEstado d = new Paquete.DelegadoEstado(paq_InformaEstado);
+                this.Invoke(d, new object[] { sender, e });
+            }
+            else
+            {
+                this.ActualizaEstado();
+            }
+        }
+
+        private void MostrarInformacion<T>(IMostrar<T> elemento)
+        {
+            if (elemento != null)
+            {
+                this.rtbMostrar.Text = elemento.MostrarDatos(elemento);
+                GuardarString.Guardar(elemento.MostrarDatos(elemento), "salida.txt");
+            }
+        }
+
+        private void cmsListas_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Paquete paquete = new Paquete(this.txtDireccion.Text, this.mtxtTrackingID.Text);
+            paquete.InformaEstado += new Paquete.DelegadoEstado(this.paq_InformaEstado);
+
+            try
+            {
+                this._correo += paquete;
+                MessageBox.Show("Paquete agregado");
+            }
+            catch (TrackingIdRepetidoException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            this.ActualizaEstado();
+        }
+
+        private void FrmPpal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this._correo.FinEntregas();
+        }
+
+        private void FrmPpal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMostrarTodos_Click(object sender, EventArgs e)
+        {
+            this.MostrarInformacion<List<Paquete>>((IMostrar<List<Paquete>>)this._correo); 
+        }
+
+        private void rtbMostrar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstEstadoEntregado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mostrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.MostrarInformacion<Paquete>((IMostrar<Paquete>)lstEstadoEntregado.SelectedItem);
+        }
+    }
+}
